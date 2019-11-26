9f0bdc5c1540ba88fb0e13363f0624e9f8967056
ast_mpozhydaiev <m.pozhydaiev@astoundcommerce.com>
2019-11-26 09:06:57 +0200

    formated code, add package comands

--- src/js/model.js
+++ src/js/model.js
@@ -1,52 +1,34 @@
-define(
-	'Model',
-	["jquery"],
-	function(){
+define('Model', ['jquery'], function() {
+  return function Model(newData) {
+    var self = this;
+    self.data = newData;
 
-		return function Model(newData){
-			var self = this;
-			self.data = newData;
-			
+    self.addItem = function(item) {
+      if (item.length === 0) {
+        return;
+      }
 
+      self.data.push(item);
+      return self.data;
+    };
+    self.removeItem = function(item) {
+      var index = self.data.indexOf(item);
 
-			self.addItem = function(item){
+      if (index === -1) {
+        return;
+      }
 
-				if(item.length === 0){
-					return;
-				}
+      self.data.splice(index, 1);
+      return self.data;
+    };
 
-				self.data.push(item);
-				return self.data;
-			};
-			self.removeItem = function(item){
-				var index = self.data.indexOf(item);
-				
-				if( index === -1){
-					return;
-				}
-
-				self.data.splice(index, 1);
-				return self.data;
-			};
-
-			self.changeItem = function(item, newValue){
-
-				var index = self.data.indexOf(item);
-				if( index === -1){
-					return;
-				}
-	 			// self.data[index] = newValue;
-	 			self.data.splice(index, 1, newValue);
-
-				console.log(self.data);
-				return self.data;
-			};
-		}
-	}
-)
-
-
-
-
-
-
+    self.changeItem = function(item, newValue) {
+      var index = self.data.indexOf(item);
+      if (index === -1) {
+        return;
+      }
+      self.data.splice(index, 1, newValue);
+      return self.data;
+    };
+  };
+});
